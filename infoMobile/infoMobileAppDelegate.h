5b8f1bb1ef2276367f82098ad8329d600b532219
JerryCrowley <jcc608@nyu.edu>
2014-04-15 11:22:31 -0400

    Initial Commit

---
+++ infoMobile/infoMobileAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  infoMobileAppDelegate.h
+//  infoMobile
+//
+//  Created by User on 4/15/14.
+//  Copyright (c) 2014 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface infoMobileAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ infoMobile/infoMobileViewController.h
@@ -0,0 +1,13 @@
+//
+//  infoMobileViewController.h
+//  infoMobile
+//
+//  Created by User on 4/15/14.
+//  Copyright (c) 2014 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface infoMobileViewController : UIViewController
+
+@end
